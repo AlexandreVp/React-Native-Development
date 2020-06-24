89f212527a500a383a4de6d44243e62f1976eed9
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2020-06-24 11:28:30 -0300

    Moving Input Logic Into A Separate Component

--- Shop 2.0/screens/user/EditProductScreen.js
+++ Shop 2.0/screens/user/EditProductScreen.js
@@ -9,9 +9,11 @@ import {
     Alert
 } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { useSelector, useDispatch } from 'react-redux';
 
 import HeaderButton from '../../components/UI/HeaderButton';
+import Input from '../../components/UI/Input'
+
+import { useSelector, useDispatch } from 'react-redux';
 import * as productsActions from '../../store/actions/products'
 
 const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE'
@@ -126,48 +128,37 @@ const EditProductScreen = props =>
     return (
         <ScrollView>
             <View style={styles.form}>
-                <View style={styles.formControl}>
-                    <Text style={styles.label}>Title</Text>
-                    <TextInput
-                        style={styles.input}
-                        value={formState.inputValues.title}
-                        onChangeText={textChangeHandler.bind(this, 'title')}
-                        keyboardType='default'
-                        autoCapitalize='sentences'
-                        autoCorrect
-                        returnKeyType='next'
-                        onEndEditing={() => {console.log('onEndEditing')}}
-                        onSubmitEditing={() => {console.log('onSubmitEditing')}}
-                    />
-                    {!formState.inputValues.title && <Text>Please enter a valid title!</Text>}
-                </View>
-                <View style={styles.formControl}>
-                    <Text style={styles.label}>Image URL</Text>
-                    <TextInput
-                        style={styles.input}
-                        value={formState.inputValues.imageUrl}
-                        onChangeText={textChangeHandler.bind(this, 'imageUrl')}
-                    />
-                </View>
+                <Input
+                    label='Title'
+                    errorText='Please enter a valid title!'
+                    keyboardType='default'
+                    autoCapitalize='sentences'
+                    autoCorrect
+                    returnKeyType='next'
+                />
+                <Input
+                    label='Image Url'
+                    errorText='Please enter a valid image url!'
+                    keyboardType='default'
+                    returnKeyType='next'
+                />
                 {editedProduct ? null : (
-                    <View style={styles.formControl}>
-                        <Text style={styles.label}>Price</Text>
-                        <TextInput
-                            style={styles.input}
-                            value={formState.inputValues.price}
-                            onChangeText={textChangeHandler.bind(this, 'price')}
-                            keyboardType='decimal-pad'
-                        />
-                    </View>
-                )}
-                <View style={styles.formControl}>
-                    <Text style={styles.label}>Description</Text>
-                    <TextInput
-                        style={styles.input}
-                        value={formState.inputValues.description}
-                        onChangeText={textChangeHandler.bind(this, 'description')}
+                    <Input
+                        label='Price'
+                        errorText='Please enter a valid price!'
+                        keyboardType='decimal-pad'
+                        returnKeyType='next'
                     />
-                </View>
+                )}
+                <Input
+                    label='Description'
+                    errorText='Please enter a valid description!'
+                    keyboardType='default'
+                    autoCapitalize='sentences'
+                    autoCorrect
+                    multiline
+                    numberOfLines={3}
+                />
             </View>
         </ScrollView>
     );
@@ -200,22 +191,7 @@ const styles = StyleSheet.create
         {
             margin: 20
         },
-        formControl: 
-        {
-            width: '100%'
-        },
-        label: 
-        {
-            fontFamily: 'open-sans-bold',
-            marginVertical: 8
-        },
-        input: 
-        {
-            paddingHorizontal: 2,
-            paddingVertical: 5,
-            borderBottomColor: '#ccc',
-            borderBottomWidth: 1
-        }
+        
     }
 );
 
