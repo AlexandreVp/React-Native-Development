c1588892a8c7888c6eedd9b050cd17c7bd58f165
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2020-06-26 23:12:09 -0300

    Fetching Products from the Server

--- Shop 3.0/store/actions/products.js
+++ Shop 3.0/store/actions/products.js
@@ -1,6 +1,42 @@
-export const DELETE_PRODUCT = 'DELETE_PRODUCT'
-export const CREATE_PRODUCT = 'CREATE_PRODUCT'
-export const UPDATE_PRODUCT = 'UPDATE_PRODUCT'
+import Product from "../../models/product";
+
+export const DELETE_PRODUCT = 'DELETE_PRODUCT';
+export const CREATE_PRODUCT = 'CREATE_PRODUCT';
+export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+export const SET_PRODUCTS = 'SET_PRODUCTS';
+
+export const fetchProducts = () => {
+
+    return async dispatch => {
+        //Here you can execute any async code you want
+        const response = await fetch(
+            'https://app-shop-d3a87.firebaseio.com/products.json'
+        );
+
+        const responseData = await response.json();
+        
+        const loadedProducts = [];
+
+        for (const key in responseData) {
+            loadedProducts.push(
+                new Product(
+                    key, 
+                    'u1', 
+                    responseData[key].title, 
+                    responseData[key].imageUrl, 
+                    responseData[key].description, 
+                    responseData[key].price
+                )
+            );
+        
+        };
+
+        dispatch({
+            type: SET_PRODUCTS,
+            products: loadedProducts
+        });
+    };
+};
 
 export const deleteProduct = productId =>
 {
