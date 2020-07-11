0e303aedff6e160d3546a336216eed853331bc23
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2020-07-11 20:19:02 -0300

    Redux and Adding Places

--- Great Places/screens/NewPlaceScreen.js
+++ Great Places/screens/NewPlaceScreen.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, StyleSheet, ScrollView, Button } from 'react-native';
 
-import Colors from '../constants/Colors'
+import Colors from '../constants/Colors';
+
+import { useDispatch } from 'react-redux';
+import * as placesActions from '../store/places-actions'
 
 const NewPlaceScreen = props => {
 
     const [titleValue, setTitleValue] = useState('');
+
+    const dispatch = useDispatch();
 
     const titleChangeHandler = text => {
         // You could add validation here
@@ -13,7 +18,8 @@ const NewPlaceScreen = props => {
     };
 
     const savePlaceHandler = () => {
-        
+        dispatch(placesActions.addPlace(titleValue));
+        props.navigation.goBack();
     };
 
     return (
