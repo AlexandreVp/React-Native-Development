377e128febcf7ae718e09d16727dabedb38b4eff
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2020-07-15 19:49:32 -0300

    Adding User Signup

--- Shop 4.0/screens/user/AuthScreen.js
+++ Shop 4.0/screens/user/AuthScreen.js
@@ -1,16 +1,83 @@
-import React from 'react';
-import { ScrollView, StyleSheet, View, KeyboardAvoidingView, Button } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient'
+import React, { useReducer, useCallback } from 'react';
+import {
+  ScrollView,
+  View,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Button
+} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
-import Input from '../../components/UI/Input'
-import Card from '../../components/UI/Card'
-import Colors from '../../constants/Colors'
+import Input from '../../components/UI/Input';
+import Card from '../../components/UI/Card';
+import Colors from '../../constants/Colors';
+
+import { useDispatch } from 'react-redux';
+import * as authActions from '../../store/actions/auth';
+
+const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE';
+
+const formReducer = (state, action) => {
+    if (action.type === FORM_INPUT_UPDATE) {
+        const updatedValues = {
+            ...state.inputValues,
+            [action.input]: action.value
+        };
+        const updatedValidities = {
+            ...state.inputValidities,
+            [action.input]: action.isValid
+        };
+        let updatedFormIsValid = true;
+        for (const key in updatedValidities) {
+            updatedFormIsValid = updatedFormIsValid && updatedValidities[key];
+        }
+        return {
+            formIsValid: updatedFormIsValid,
+            inputValidities: updatedValidities,
+            inputValues: updatedValues
+        };
+    }
+    return state;
+};
 
 const AuthScreen = props => {
+    
+    const dispatch = useDispatch();
 
+    const [formState, dispatchFormState] = useReducer(formReducer, {
+        inputValues: {
+            email: '',
+            password: ''
+        },
+        inputValidities: {
+            email: false,
+            password: false
+        },
+        formIsValid: false
+    });
+
+    const signupHandler = () => {
+        dispatch(
+            authActions.signup(
+                formState.inputValues.email,
+                formState.inputValues.password
+            )
+        );
+    };
+
+    const inputChangeHandler = useCallback(
+        (inputIdentifier, inputValue, inputValidity) => {
+            dispatchFormState({
+                type: FORM_INPUT_UPDATE,
+                value: inputValue,
+                isValid: inputValidity,
+                input: inputIdentifier
+            });
+        },
+        [dispatchFormState]
+    );
 
     return (
-
         <KeyboardAvoidingView
             behavior='height'
             keyboardVerticalOffset={50}
@@ -19,39 +86,39 @@ const AuthScreen = props => {
             <LinearGradient colors={['#ffedff', '#ffe3ff']} style={styles.gradient}>
                 <Card style={styles.authContainer}>
                     <ScrollView>
-                        <Input 
-                            id='email'
-                            label='E-mail'
-                            keyboardType='email-address'
+                        <Input
+                            id="email"
+                            label="E-Mail"
+                            keyboardType="email-address"
                             required
                             email
-                            autoCaptalize='none'
-                            errorMessage='Please enter a valid email address.'
-                            onInputChange={() => {}}
-                            initialValue=''
+                            autoCapitalize="none"
+                            errorText="Please enter a valid email address."
+                            onInputChange={inputChangeHandler}
+                            initialValue=""
                         />
-                        <Input 
-                            id='password'
-                            label='Password'
-                            keyboardType='default'
+                        <Input
+                            id="password"
+                            label="Password"
+                            keyboardType="default"
                             secureTextEntry
                             required
                             minLength={5}
-                            autoCaptalize='none'
-                            errorMessage='Please enter a valid password.'
-                            onInputChange={() => {}}
-                            initialValue=''
+                            autoCapitalize="none"
+                            errorText="Please enter a valid password."
+                            onInputChange={inputChangeHandler}
+                            initialValue=""
                         />
                         <View style={styles.buttonContainer}>
-                            <Button 
-                                title='Login'
+                            <Button
+                                title="Login"
                                 color={Colors.primary}
-                                onPress={() => {}}
+                                onPress={signupHandler}
                             />
                         </View>
                         <View style={styles.buttonContainer}>
-                            <Button 
-                                title='Switch to Sign Up'
+                            <Button
+                                title="Switch to Sign Up"
                                 color={Colors.accent}
                                 onPress={() => {}}
                             />
@@ -60,22 +127,21 @@ const AuthScreen = props => {
                 </Card>
             </LinearGradient>
         </KeyboardAvoidingView>
-
     );
 };
 
 AuthScreen.navigationOptions = {
     headerTitle: 'Authenticate'
-}
+};
 
 const styles = StyleSheet.create({
     screen: {
-        flex: 1,
+        flex: 1
     },
     gradient: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'center'
     },
     authContainer: {
         width: '80%',
@@ -84,8 +150,8 @@ const styles = StyleSheet.create({
         padding: 20
     },
     buttonContainer: {
-        marginTop: 10,
-    },
+        marginTop: 10
+    }
 });
 
-export default AuthScreen
+export default AuthScreen;
