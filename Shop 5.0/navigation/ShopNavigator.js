cd217d30097e6d5c945a92d8e0815006ea228fee
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2020-07-24 19:02:49 -0300

    Migrating the Drawer Navigation

--- Shop 5.0/navigation/ShopNavigator.js
+++ Shop 5.0/navigation/ShopNavigator.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Platform, SafeAreaView, Button, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer'
+import { createDrawerNavigator, DrawerItemList } from '@react-navigation/drawer'
 
 import { useDispatch } from 'react-redux';
 import * as authActions from '../store/actions/auth';
@@ -159,6 +159,79 @@ export const AdminNavigator = () => {
 // 		defaultNavigationOptions: defaultNavOptions
 // 	}
 // );
+
+const ShopDrawerNavigator = createDrawerNavigator();
+
+const ShopNavigator = () => {
+
+	const dispatch = useDispatch();
+
+	return (
+		<ShopDrawerNavigator.Navigator
+			drawerContent={props => {
+				return (
+					<View style={{flex: 1, padding: 20}}>
+						<SafeAreaView forceInset={{top: 'always', horizontal: 'never'}} >
+							<DrawerItemList 
+								{...props}
+							/>
+							<Button title='Logout' color={Colors.primary} onPress={() => {
+								dispatch(
+									authActions.logout()
+								);
+							}}/>
+						</SafeAreaView>
+					</View>
+				)
+			}}
+			drawerContentOptions={{
+				activeTintColor: Colors.primary,
+			}}
+		>
+
+			<ShopDrawerNavigator.Screen 
+				name='Products'
+				component={ProductsNavigator}
+				options={{
+					drawerIcon: props => (
+						<Ionicons 
+							name={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+							size={23}
+							color={props.color}
+						/>
+					)
+				}}
+			/>
+			<ShopDrawerNavigator.Screen 
+				name='Orders'
+				component={OrdersNavigator}
+				options={{
+					drawerIcon: props => (
+						<Ionicons 
+							name={Platform.OS === 'android' ? 'md-list' : 'ios-list'}
+							size={23}
+							color={props.color}
+						/>
+					)
+				}}
+			/>
+			<ShopDrawerNavigator.Screen 
+				name='Admin'
+				component={AdminNavigator}
+				options={{
+					drawerIcon: props => (
+						<Ionicons 
+							name={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+							size={23}
+							color={props.color}
+						/>
+					)
+				}}
+			/>
+
+		</ShopDrawerNavigator.Navigator>
+	)
+};
 
 const ShopNavigator = createDrawerNavigator(
 	{
