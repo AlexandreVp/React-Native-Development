a8d0f7315672d68015cb1e2c4c4f8c6424cf09cd
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2020-08-19 19:16:01 -0300

    Fetching Data from the Local Database

--- Great Places/screens/PlacesListScreen.js
+++ Great Places/screens/PlacesListScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet, Platform, FlatList } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
@@ -6,11 +6,18 @@ import HeaderButton from '../components/HeaderButton';
 import PlaceItem from '../components/PlaceItem';
 import Colors from '../constants/Colors';
 
-import { useSelector } from 'react-redux';
+import * as placesActions from '../store/places-actions';
+import { useSelector, useDispatch } from 'react-redux';
 
 const PlacesListScreen = props => {
 
+    const dispatch = useDispatch();
+
     const places = useSelector(state => state.places.places);
+
+    useEffect(() => {
+        dispatch(placesActions.loadPlaces());
+    }, [dispatch]);
 
     return (
         <FlatList 
