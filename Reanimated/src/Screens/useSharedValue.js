6c71c83408a931cfc51c49d718834693fb20abef
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2020-12-21 16:11:48 -0300

    useSharedValue

--- Reanimated/src/Screens/useSharedValue.js
+++ Reanimated/src/Screens/useSharedValue.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import Animated, { useSharedValue } from 'react-native-reanimated';
 
-const useSharedValue = () => {
+import Colors from '../Constants/Colors';
+
+const UseSharedValue = () => {
+
+    const [wState, setWState] = useState(50)
+
+    const width = useSharedValue(wState);
+    
+    // console.log(width.value);
 
     return (
         <View style={styles.container}>
-            <Text>oi</Text>
+            <View style={{...styles.view, width: width.value}} />
+            <RectButton style={styles.button} onPress={() => {
+                // width.value = Math.random() * 300
+                setWState(Math.random() * 300)
+            }}>
+                <Text style={{color: 'white'}}>Button</Text>
+            </RectButton>
         </View>
     );
 }
@@ -19,7 +35,20 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    view: {
+        height: 30,
+        marginBottom: 100,
+        backgroundColor: Colors.primary
+    },
+    button: {
+        width: 100,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#75c',
+        borderRadius: 20,
     }
 })
 
-export default useSharedValue;
+export default UseSharedValue;
