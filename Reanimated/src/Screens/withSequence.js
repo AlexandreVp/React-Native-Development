ab3c69ed7b2ebd57c80fb9323fa55b274de1e1e1
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2020-12-24 11:46:42 -0300

    WithSequence bug correction

--- Reanimated/src/Screens/withSequence.js
+++ Reanimated/src/Screens/withSequence.js
@@ -13,31 +13,35 @@ import Colors from '../Constants/Colors';
 
 const WithSequence = () => {
 
-    const width = useSharedValue(150);
+    const width = useSharedValue(50);
 
     const boxStyle = useAnimatedStyle(() => {
         return {
-            width: withSequence(
-                withTiming(width.value, {
-                    duration: 300,
-                    easing: Easing.bezier(0.25, 0.1, 0.25, 1),
-                }),
-                withTiming(width.value - 100, {
-                    duration: 300,
-                    easing: Easing.bezier(0.25, 0.1, 0.25, 1),
-                }),
-                withTiming(width.value, {
-                    duration: 300,
-                    easing: EasingNode.bezier(0.25, 0.1, 0.25, 1),
-                }),
-            ),
-        }
-    })
+            width: width.value
+        };
+    });
+
+    const animate = () => {
+        width.value = withSequence(
+            withTiming(200, {
+                duration: 300,
+                easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+            }),
+            withTiming(125, {
+                duration: 300,
+                easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+            }),
+            withTiming(300, {
+                duration: 300,
+                easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+            })
+        )
+    };
 
     return (
         <View style={styles.container}>
             <Animated.View style={[styles.box, boxStyle]} />
-            <Button onPress={() => (width.value = Math.random()*100 + 200)} title="Press me" color={Colors.secondary}/>
+            <Button onPress={animate} title="Press me" color={Colors.secondary}/>
         </View>
     );
 }
