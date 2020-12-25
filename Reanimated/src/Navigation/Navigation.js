301f7abe9020c1ccb196cf9ec7921d78c5cbaf70
AlexandreVp <47641623+AlexandreVp@users.noreply.github.com>
2020-12-25 13:49:50 -0300

    useAnimatedProps

--- Reanimated/src/Navigation/Navigation.js
+++ Reanimated/src/Navigation/Navigation.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Reanimated from "../Screens/Reanimated";
 import Drag, { screenOptions as dragScreenOptions } from '../Screens/Drag';
 import Scroll from "../Screens/Scroll";
+import useAnimatedProps from '../Screens/useAnimatedProps';
 import useAnimatedReaction from '../Screens/useAnimatedReaction';
 import useAnimatedScrollHandler from '../Screens/useAnimatedScrollHandler';
 import useAnimatedStyle, { screenOptions as useAnimatedStyleOptions } from '../Screens/useAnimatedStyle';
@@ -63,6 +64,12 @@ export const AppNavigator = () => {
             <DrawerNavigator.Screen 
                 name='Scroll'
                 component={Scroll}
+            />
+
+            <DrawerNavigator.Screen 
+                name='useAnimatedProps'
+                component={useAnimatedProps}
+                options={defaultNavOptions}
             />
 
             <DrawerNavigator.Screen 
